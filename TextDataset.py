8b57c93662b511e7ce6d107d535ef9f25101e8d6
azqpeng <azqpeng@gmail.com>
2023-05-23 23:42:18 -0400

    new mask

--- TextDataset.py
+++ TextDataset.py
@@ -3,6 +3,8 @@ import pandas as pd
 from torch_geometric.data import InMemoryDataset, Data
 from sklearn.model_selection import train_test_split
 from build_graph import build_graph
+import torch_geometric.transforms as T
+
 
 class TextDataset(InMemoryDataset):
     def __init__(self, docList, labels, nodeCount, test_size, window = 5, transform = None):
@@ -17,27 +19,30 @@ class TextDataset(InMemoryDataset):
         # create adjacency matrix
         self.adj = build_graph(docList, "PMI", window = window)
 
-        # split data into training, validation(?), test
-        train_data, test_data, train_labels, test_labels = train_test_split(pd.Series(docList), pd.Series(labels), test_size = test_size)
+        split = T.RandomNodeSplit(num_val=0.1, num_test=0.2)
+        self.data = split(data)
 
-        self.x_train = train_data
-        self.x_test = test_data
-        self.y_train = train_labels
-        self.y_test = test_labels
+        # # split data into training, validation(?), test
+        # train_data, test_data, train_labels, test_labels = train_test_split(pd.Series(docList), pd.Series(labels), test_size = test_size)
+
+        # self.x_train = train_data
+        # self.x_test = test_data
+        # self.y_train = train_labels
+        # self.y_test = test_labels
 
         
-        # create masks
-        self.train_mask = torch.zeros(nodeCount, dtype = torch.bool)
-        self.test_mask = torch.zeros(nodeCount, dtype = torch.bool)
+        # # create masks
+        # self.train_mask = torch.zeros(nodeCount, dtype = torch.bool)
+        # self.test_mask = torch.zeros(nodeCount, dtype = torch.bool)
 
-        self.train_mask[train_data.index + vocabCount] = True
-        self.test_mask[test_data.index + vocabCount] = True
+        # self.train_mask[train_data.index + vocabCount] = True
+        # self.test_mask[test_data.index + vocabCount] = True
 
-        data['train_mask'] = self.train_mask
-        data['test_mask'] = self.test_mask
+        # data['train_mask'] = self.train_mask
+        # data['test_mask'] = self.test_mask
 
-        # whatever the fuck slices are
-        self.data, self.slices = self.collate([data])
+        # # whatever the fuck slices are
+        # self.data, self.slices = self.collate([data])
 
     def _download(self):
         return
